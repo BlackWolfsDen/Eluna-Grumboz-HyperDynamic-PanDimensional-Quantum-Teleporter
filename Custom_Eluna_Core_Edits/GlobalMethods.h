21fa04260d043c3f0d571b8a7dbc94b7918a3e62
Grumbo <blackwolfsden@hotmail.com>
2020-05-23 22:17:56 -0600

    Update GlobalMethods.h

--- Custom_Eluna_Core_Edits/GlobalMethods.h
+++ Custom_Eluna_Core_Edits/GlobalMethods.h
@@ -7,8 +7,18 @@
        * - Grumbo Was HERE !!
        * @return uint32 realm ID
        */
+  #if defined(MANGOS)
       int GetRealmID(lua_State* L)
       {
           Eluna::Push(L, eWorld->GetRealmID());
           return 1;
       }
+	#endif
+
+  #if defined(TRINITY)
+      int GetRealmID(lua_State* L)
+      {
+          Eluna::Push(L, rbac::RBACData::_realmId);
+          return 1;
+      }
+	#endif
