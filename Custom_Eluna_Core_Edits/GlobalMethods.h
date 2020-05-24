295e5b4f65656aba6bc5c745660e54ed25ef5888
Grumbo <blackwolfsden@hotmail.com>
2020-05-23 21:49:35 -0600

    Update GlobalMethods.h

--- Custom_Eluna_Core_Edits/GlobalMethods.h
+++ Custom_Eluna_Core_Edits/GlobalMethods.h
@@ -1,15 +1,5 @@
-  #if defined(MANGOS) || (CMANGOS)
-      /**
-       * Returns emulator mangosd.conf RealmID
-       *
-       * - For cMaNGOS returns the realmID as it is in the conf file.
-       * - for MaNGOS returns the realmID as it is in the conf file.
-       * - Grumbo Was HERE !!
-       * @return Realm Uint ID
-       */
-      int GetRealmID(lua_State* L)
+     int GetRealmID(lua_State* L)
       {
           Eluna::Push(L, eWorld->GetRealmID());
           return 1;
       }
-  #endif
