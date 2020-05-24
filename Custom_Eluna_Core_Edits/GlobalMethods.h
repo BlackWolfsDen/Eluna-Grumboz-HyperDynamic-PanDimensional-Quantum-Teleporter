e22ac9acd4ae5c21db3f8933fbda79e270193143
Grumbo <blackwolfsden@hotmail.com>
2020-05-23 22:11:01 -0600

    Update GlobalMethods.h

--- Custom_Eluna_Core_Edits/GlobalMethods.h
+++ Custom_Eluna_Core_Edits/GlobalMethods.h
@@ -1,4 +1,13 @@
-     int GetRealmID(lua_State* L)
+      /**
+       * Returns emulator .conf RealmID
+       *
+       * - For cMaNGOS returns the realmID as it is in the conf file.
+       * - for MaNGOS returns the realmID as it is in the conf file.
+       * - for TrinityCore returns the realmID as it is in the conf file.
+       * - Grumbo Was HERE !!
+       * @return uint32 realm ID
+       */
+      int GetRealmID(lua_State* L)
       {
           Eluna::Push(L, eWorld->GetRealmID());
           return 1;
